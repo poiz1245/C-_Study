4135d442ae9fc03e279eb9a6c7aa57755d82699b
poiz1245 <139199211+poiz1245@users.noreply.github.com>
2024-04-25 18:10:23 +0900

    Array예제 추가

---
+++ Array/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 80, 70, 60, 90, 100 };
+            int sum = CalculateSum(scores); //int타입의 배열 전달
+
+            Console.WriteLine(sum);   
+        }
+
+        static int CalculateSum(int[] scoresArray) //int타입의 배열을 매개로 받음
+        {
+            int sum = 0;
+            for(int i = 0; i < scoresArray.Length; i++)
+            {
+                sum += scoresArray[i];
+            }
+
+            return sum;
+        }
+    }
+}
